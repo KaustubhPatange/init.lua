f22b16960dc8e80290456b329bf21c488231546b
KaustubhPatange <developerkp16@gmail.com>
2023-11-14 20:40:50 +0530

    fix keymap bug for nvim cmp causing to it spawn "<" to a keymap

--- lua/kp/plugins/config/lsp.lua
+++ lua/kp/plugins/config/lsp.lua
@@ -91,7 +91,7 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
     ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<cr>"] = cmp.mapping.confirm { select = true },
-    ["<C-space"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<C-space>"] = cmp.mapping.complete(),
   },
 }
