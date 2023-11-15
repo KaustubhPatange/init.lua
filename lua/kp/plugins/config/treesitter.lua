4b9e0528247949182df4f91901fceb6ed20d94ef
KaustubhPatange <developerkp16@gmail.com>
2023-11-15 09:53:02 +0530

    local opts

--- lua/kp/plugins/config/treesitter.lua
+++ lua/kp/plugins/config/treesitter.lua
@@ -1,18 +1,33 @@
-opts = {
+local opts = {
   highlight = { enable = true },
   auto_install = false,
   incremental_selection = { enable = true },
   autotag = { enable = true },
-  context_commentstring = { enable = true, enable_autocmd = false },
-  ensure_installed = {"lua", "javascript", "typescript", "python"},
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  ensure_installed = { "lua", "javascript", "typescript", "python" },
 }
 
-require('Comment').setup()
+require("ts_context_commentstring").setup {}
 require("nvim-treesitter.configs").setup(opts)
-    
+
+local comment = require "Comment"
+comment.setup {
+  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+}
+
 -- Mappings (Comment)
-nnoremap("<leader>/", function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end, "Comment line")
-vnoremap("<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", "Toggle comment line")
+nnoremap(
+  "<leader>/",
+  function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end,
+  "Comment line"
+)
+vnoremap(
+  "<leader>/",
+  "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+  "Toggle comment line"
+)
 
 -- Mappings (Treesitter)
-
