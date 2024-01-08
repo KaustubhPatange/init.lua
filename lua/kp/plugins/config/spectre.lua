88906c3f00648dfc7435fa2ee0c7d9580ff82aa3
KaustubhPatange <developerkp16@gmail.com>
2024-01-08 15:05:40 +0530

    add remap for nvim spectre in visual mode

--- lua/kp/plugins/config/spectre.lua
+++ lua/kp/plugins/config/spectre.lua
@@ -1,4 +1,12 @@
-nnoremap("<leader>fR", 
+-- Mappings
+
+nnoremap(
+  "<leader>fR",
   "<cmd>lua require('spectre').open_visual({select_word=true})<cr>",
+  "Search and replace current word"
+)
+vnoremap(
+  "<leader>fR",
+  "<cmd>lua require('spectre').open_visual({select_word=false})<cr>",
   "Search and replace current word"
 )
