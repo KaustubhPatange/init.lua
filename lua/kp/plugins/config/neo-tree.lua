4a1d3888e479ab7344af7d4254551b758e554896
KaustubhPatange <developerkp16@gmail.com>
2024-01-21 12:26:44 +0530

    add neotree copy path to clipboard

--- lua/kp/plugins/config/neo-tree.lua
+++ lua/kp/plugins/config/neo-tree.lua
@@ -97,7 +97,35 @@ require("neo-tree").setup {
   -- A list of functions, each representing a global custom command
   -- that will be available in all sources (if not overridden in `opts[source_name].commands`)
   -- see `:h neo-tree-custom-commands-global`
-  commands = {},
+  commands = {
+    copy_selector = function(state)
+      local node = state.tree:get_node()
+      local filepath = node:get_id()
+      local filename = node.name
+      local modify = vim.fn.fnamemodify
+
+      local vals = {
+        ["BASENAME"] = modify(filename, ":r"),
+        ["EXTENSION"] = modify(filename, ":e"),
+        ["FILENAME"] = filename,
+        ["PATH (CWD)"] = modify(filepath, ":."),
+        ["PATH (HOME)"] = modify(filepath, ":~"),
+        ["PATH"] = filepath,
+        ["URI"] = vim.uri_from_fname(filepath),
+      }
+
+      local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+      if vim.tbl_isempty(options) then return end
+      table.sort(options)
+      vim.ui.select(options, {
+        prompt = "Choose to copy to clipboard:",
+        format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
+      }, function(choice)
+        local result = vals[choice]
+        if result then vim.fn.setreg("+", result) end
+      end)
+    end,
+  },
   window = {
     position = "left",
     width = 40,
@@ -157,6 +185,7 @@ require("neo-tree").setup {
       ["<"] = "prev_source",
       [">"] = "next_source",
       ["i"] = "show_file_details",
+      ["Y"] = "copy_selector",
     },
   },
   nesting_rules = {},
