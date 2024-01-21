9436e5582bf7e129abfdee8cb2469efd4309e2dc
KaustubhPatange <developerkp16@gmail.com>
2024-01-21 12:28:30 +0530

    lsp workspace diagnostics + trouble + theme

--- lua/kp/plugins/config/lsp.lua
+++ lua/kp/plugins/config/lsp.lua
@@ -108,6 +108,17 @@ require("mason-lspconfig").setup {
   ensure_installed = lsp_servers,
   handlers = {
     lsp_zero.default_setup,
+    pyright = function()
+      require("lspconfig").pyright.setup {
+        settings = {
+          python = {
+            analysis = {
+              diagnosticMode = "workspace",
+            },
+          },
+        },
+      }
+    end,
     lua_ls = function()
       local lua_opts = lsp_zero.nvim_lua_ls()
       require("lspconfig").lua_ls.setup(lua_opts)
