7ff2f63d8fa4a565dfa633d5d91ac83ff11694c7
KaustubhPatange <developerkp16@gmail.com>
2024-01-21 16:21:25 +0530

    no need for session restoration

--- lua/kp/plugins/config/bufferline.lua
+++ lua/kp/plugins/config/bufferline.lua
@@ -26,13 +26,6 @@ local opts = {
 
 require("bufferline").setup(opts)
 
--- Fix bufferline when restoring a session
-vim.api.nvim_create_autocmd("BufAdd", {
-  callback = function()
-    vim.schedule(function() pcall(nvim_bufferline) end)
-  end,
-})
-
 -- Mappings (mini.bufremove)
 local function delete_buffer()
   local bd = require("mini.bufremove").delete
