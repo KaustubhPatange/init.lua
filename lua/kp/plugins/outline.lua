4d9abd9a055c6c409ac8f77d748bbf03015af311
KaustubhPatange <developerkp16@gmail.com>
2025-01-07 13:17:37 +0530

    outline nvim instead of aerial

---
+++ lua/kp/plugins/outline.lua
@@ -0,0 +1,11 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = { -- Example mapping to toggle outline
+    { "<leader>lS", "<cmd>Outline<CR>", desc = "Symbols outline" },
+  },
+  opts = {
+    -- Your setup opts here
+  },
+}
