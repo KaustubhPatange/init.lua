32b7daac58e146d6012507ca661df5a6b63e97be
KaustubhPatange <developerkp16@gmail.com>
2025-02-01 22:42:21 +0530

    remove icons mapping

--- lua/kp/plugins/which-key.lua
+++ lua/kp/plugins/which-key.lua
@@ -9,5 +9,9 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    icons = {
+      mappings = false
+
+    }
   }
 }
